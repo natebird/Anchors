00ffc6ff6e1c7c5959036fd894a9667f64989598
Nate Bird <natebird@gmail.com>
2016-04-07 10:33:34 -0400

    Initial commit of sample project without storyboards

---
+++ Anchors/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Anchors
+//
+//  Created by Nate Bird on 4/7/16.
+//  Copyright © 2016 Birdhouse. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow? = UIWindow()
+
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    window?.rootViewController = ViewController()
+    window?.makeKeyAndVisible()
+    return true
+  }
+
+}
+
---
+++ Anchors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Anchors
+//
+//  Created by Nate Bird on 4/7/16.
+//  Copyright © 2016 Birdhouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+}
+
